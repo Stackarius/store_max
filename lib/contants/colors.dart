5b6b92302bf7471b00340423b901692675acdaab
Adepoju Opeyemi <opepoju@gmail.com>
2025-10-25 22:09:38 +0100

    feat: add color variables

---
+++ lib/contants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF000000);
+  static const Color secondary = Color(0xFFFFFFFF);
+  static const Color yellow = Color.fromARGB(255, 252, 235, 2);
+  static const Color aliceBlue = Color.fromARGB(255, 236, 244, 252);
+  static const Color myGrey = Color.fromARGB(255, 114, 114, 114);
+  static const Color error = Color.fromARGB(255, 230, 0, 0);
+  static const Color success = Color(0xFF4CAF50);
+}
