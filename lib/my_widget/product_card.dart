026a53b1e358a3caa714a38c013222a27091d5a5
Adepoju Opeyemi <opepoju@gmail.com>
2025-10-25 22:32:10 +0100

    feat: create product card widget for grid display

---
+++ lib/my_widget/product_card.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:store_max/contants/colors.dart';
+import 'package:store_max/models/product.dart';
+
+import '../pages/product_detail.dart';
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+  const ProductCard({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.secondary,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: InkWell(
+        onTap: () {
+          // Navigate to product detail page
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ProductDetail(product: product),
+            ),
+          );
+        },
+        borderRadius: BorderRadius.circular(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // IMAGE
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12),
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12),
+                  ),
+
+                  child:
+                      product.imagePath != null && product.imagePath!.isNotEmpty
+                          ? Image.asset(
+                            product.imagePath!,
+                            fit: BoxFit.cover,
+                            errorBuilder:
+                                (context, error, stackTrace) => const Center(
+                                  child: Icon(
+                                    Icons.inventory_2,
+                                    size: 40,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                          )
+                          : const Center(
+                            child: Icon(
+                              Icons.inventory_2,
+                              size: 40,
+                              color: Colors.grey,
+                            ),
+                          ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.name,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    maxLines: 1,
+                    overflow: TextOverflow.fade,
+                  ),
+                  SizedBox(height: 5),
+                  // price
+                  Text(
+                    '₦ ${product.price.toStringAsFixed(2)}',
+                    style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+                  ),
+                  Text(
+                    'Qty: ${product.quantity}',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: AppColors.primary.withAlpha(150),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
